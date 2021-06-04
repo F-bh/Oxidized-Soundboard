962aee6d53fe06a4c2a9d71759da97e72f1bb734
F-bh <72809273+F-bh@users.noreply.github.com>
2021-06-04 18:07:04 +0200

    added simple Audio controls - input/output loudness + mute buttons

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use iced::{
-    button, Align, Button, Column, Element, Length, Sandbox, Settings, Text
+    button, Align, Button, Column,
+    Element, Length, Sandbox, Settings,
+    Text, slider, Row,
 };
+use std::ops::RangeInclusive;
+use crate::AudioSettingsMessage::MutePressed;
 
 fn main() -> iced::Result{
     Example::run(Settings{
@@ -8,15 +12,40 @@ fn main() -> iced::Result{
         ..Settings::default()
     })
 }
-
+#[derive(Default)]
 struct Example{
-    button_state: button::State,
-    button_text: String,
+    audio: AudioSettings
 }
 
-enum Message{
-    Proggers
+#[derive(Default)]
+struct AudioSettings{
+    output_slider_value: i32,
+    output_slider: slider::State,
+    output_mute_button: button::State,
+    output_muted: bool,
+    input_slider_value: i32,
+    input_slider: slider::State,
+    input_mute_button: button::State,
+    input_muted: bool,
 }
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    AudioSettings(AudioSettingsMessage)
+}
+
+#[derive(Debug, Clone, Copy)]
+enum AudioType{
+    Input,
+    Output
+}
+
+#[derive(Debug, Clone, Copy)]
+enum AudioSettingsMessage{
+    SliderChange(i32, AudioType),
+    MutePressed(AudioType),
+}
+
 
 impl Sandbox for Example{
     type Message = Message;
@@ -26,21 +55,107 @@ impl Sandbox for Example{
     }
 
     fn title(&self) -> String {
-        String::from("Hello World")
+        String::from("Oxidized-Soundboard")
     }
 
     fn update(&mut self, message: Self::Message) {
-        self.button_text = String::from("KEKW");
+        match message{
+           Message::AudioSettings(msg) =>{
+                match msg{
+                   AudioSettingsMessage::SliderChange(val, Type) => {
+                       match Type {
+                           AudioType::Input => {
+                               self.audio.input_slider_value = val
+                           }
+                           AudioType::Output => {
+                               self.audio.output_slider_value = val
+                           }
+                       }
+                   }
+                   AudioSettingsMessage::MutePressed(Type) => {
+                       match Type{
+                           AudioType::Input => {
+                               println!("MutePressed {}","in")
+                           }
+                           AudioType::Output => {
+                               println!("MutePressed {}","out")
+                           }
+                       }
+                    }
+                }
+            }
+        }
     }
 
     fn view(&mut self) -> Element<'_, Self::Message> {
-        Column::new()
+       Column::new()
             .padding(20)
             .align_items(Align::Center)
             .push(
-                Button::new(&mut self.button_state, &self.button_text)
-                    .on_press(Message::Proggers),
+                audio_settings_view(self)
             )
             .into()
     }
+}
+
+
+fn audio_settings_view(state: &mut Example) -> Element<'_, Message>{
+    Column::new()
+        .padding(10)
+        .push(
+        Row::new()
+            .padding(20)
+            .align_items(Align::Start)
+            //add output controls
+            .push(
+                slider::Slider::new(
+                        &mut state.audio.input_slider,
+                        RangeInclusive::new(0, 100),
+                        state.audio.input_slider_value,
+                        audio_slider_change(AudioType::Input)
+                    )
+                    .step(1)
+            )
+            .push(
+                Text::new(state.audio.input_slider_value.to_string())
+            )
+            .push(
+                Button::new(&mut state.audio.input_mute_button, Text::new("mute input"))
+                    .on_press(Message::AudioSettings(AudioSettingsMessage::MutePressed(AudioType::Input)))
+            )
+        )
+        //add output controls
+        .push(
+            Row::new()
+                .padding(20)
+                .align_items(Align::Start)
+                .push(
+                    slider::Slider::new(
+                            &mut state.audio.output_slider,
+                            RangeInclusive::new(0, 100),
+                            state.audio.output_slider_value,
+                            audio_slider_change(AudioType::Output)
+                        )
+                        .step(1)
+                )
+                .push(
+                    Text::new(state.audio.output_slider_value.to_string())
+                )
+                .push(
+                    Button::new(&mut state.audio.output_mute_button, Text::new("mute output"))
+                        .on_press(Message::AudioSettings(AudioSettingsMessage::MutePressed(AudioType::Output)))
+                )
+        )
+        .into()
+}
+
+//function builder that returns an onChanged function depending on the audio_type
+fn audio_slider_change(audio_type: AudioType) -> fn(i32) -> Message {
+    return match audio_type {
+        AudioType::Input =>
+            |val: i32| Message::AudioSettings(AudioSettingsMessage::SliderChange(val, AudioType::Input)),
+        AudioType::Output =>
+            |val: i32| Message::AudioSettings(AudioSettingsMessage::SliderChange(val, AudioType::Output))
+    }
+
 }
